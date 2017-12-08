0944c8b38dca2e9549997a6cf3f6e917773642bc
sylvainthery <sylvain.thery@icube.unistra.fr>
2017-12-08 17:16:50 +0100

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,15 @@ $(document).ready(function(){
 		topSpacing: 0,
 		widthFromWrapper: false
 	});
+
+        imgArray = ["url(assets/img/quilt1.png)",
+                    "url(assets/img/quilt2.png)",
+                    "url(assets/img/quilt3.png)",
+                    "url(assets/img/quilt4.png)"];
+        randomNumber = Math.floor((Math.random() * imgArray.length));
+        imgh = imgArray[randomNumber] + " no-repeat 400px 20px";
+	$("#header_wrap").css('background',imgh);
+
+       // $("#main_content_wrap").css('background','url(assets/img/lena_big.jpg)');
+	// no-repeat scroll 16px 46px transparent
 });
